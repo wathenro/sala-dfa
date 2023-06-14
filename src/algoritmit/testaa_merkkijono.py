d26020e491647494103cfb7fe95158304591df85
wathero <rolf.wathen@gmail.com>
2023-06-14 09:54:31 +0300

    toimii paremmin

--- src/algoritmit/testaa_merkkijono.py
+++ src/algoritmit/testaa_merkkijono.py
@@ -1,4 +1,8 @@
 class testaa_merkkijono():
     def __init__(self,merkkijono) -> None:
+        """Konstruktori
+        
+        Tarkastetaan että syötetty merkkijono on analyysikelpoinen.
+        """
         self.on_validi=merkkijono.isprintable()
         
